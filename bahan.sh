0543a8a9aacc76f03d802c4dd86316990ad7ab75
BgLL976 <75918094+BgLL976@users.noreply.github.com>
2020-12-13 10:05:11 +0800

    Add files via upload

---
+++ bahan.sh
@@ -0,0 +1,27 @@
+clear
+echo
+echo "Sabar Yeah Shayank..."
+sleep 3
+echo
+pkg install python
+clear
+pkg install filet
+clear
+pkg install ruby -y
+clear
+gem install lolcat
+clear
+figlet Hanief Aktivisme | lolcat
+sleep 2
+clear
+figlet Hanief Aktivisme | lolcat
+sleep 2
+pip2 install -r requirements.txt
+clear
+pip2 install mechanize
+clear
+pip2 install requests
+clear
+echo "Tunggu... Lagi Colmek Bentar"
+sleep 3
+python2 SlaveFb.py
